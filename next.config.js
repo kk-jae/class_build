0f99cbcd8a9657e67200dcb32ddce61362b99957
권현재 <kk_jae@naver.com>
2023-03-09 10:53:53 +0900

    분기배포

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "kwon-deploy-project",
+
+  // 아래의 주소들만 out 폴더에 만들어줘!! (getServerSideProps 있는 페이지는 제외)
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -10,3 +10,8 @@ export default function BoardPage() {
     </>
   );
 }
+
+export const getSeverSideProps = () => {
+  // 만약 서버사이드 렌더링을 하는 페이지라면?? out 폴더 생성 불가
+  //                                         ==> 이런 경우, next.config.js에서 exportPathMap으로 현재페이지 제외시키기
+};
